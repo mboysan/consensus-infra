8c1a59b1cb5c3bb64ec0b65a90308f94879648c1
mboysan <dev@mboysan.com>
2024-04-08 11:31:55 +0300

    THESIS-T-105: aggregate message count/sizes

--- aws/orchestrate/playbooks/remote_data_scripts/analyze_store_message_metrics.R
+++ aws/orchestrate/playbooks/remote_data_scripts/analyze_store_message_metrics.R
@@ -40,9 +40,10 @@ data$metric_name <- sub(".*\\.", "", data$metric_name)
 info("Plotting message counts")
 # Count the number of messages per group
 message_counts <- data %>% summarise(count = n())
-
 # order by timestamp
 message_counts <- message_counts %>% arrange(timestamp_sec)
+# aggregate counts by timestamp and test_id
+message_counts <- aggregate(count ~ timestamp_sec + test_id, message_counts, sum)
 
 # Plot count of messages, grouped by consensusAlg & timestamp_sec
 ggplot(message_counts, aes(x = timestamp_sec, y = count, color = test_id)) +
@@ -57,9 +58,10 @@ rm(message_counts); gc()
 info("Plotting message sizes")
 # Sum the size of messages per group
 message_sizes <- data %>% summarise(sum = sum(metric_value / 1024))
-
 # order by timestamp
 message_sizes <- message_sizes %>% arrange(timestamp_sec)
+# aggregate sum by timestamp and test_id
+message_sizes <- aggregate(sum ~ timestamp_sec + test_id, message_sizes, sum)
 
 # Plot size of messages, grouped by consensusAlg & timestamp_sec
 ggplot(message_sizes, aes(x = timestamp_sec, y = sum, color = test_id)) +
