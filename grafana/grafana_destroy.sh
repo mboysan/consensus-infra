d403431227a36a66ad4aa10fa09e3c74baca74b8
mboysan <dev@mboysan.com>
2024-10-13 13:02:22 +0300

    THESIS-111: keep docker grafana_data volume

--- grafana/grafana_destroy.sh
+++ grafana/grafana_destroy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-echo "Copying grafana dashboards..."
-docker cp grafana:/var/lib/grafana/dashboards/dashboard-main.json ./config/grafana_dashboards/work/dashboard-main-$(date +%d%m%Y_%H%M).json
-
 echo "Stopping containers..."
 docker compose down
 
 echo "Removing volumes..."
 docker volume prune -f
-docker volume rm grafana_grafana_data
 docker volume rm grafana_influxdb_data
+#docker volume rm grafana_grafana_data
